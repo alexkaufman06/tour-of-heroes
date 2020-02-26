3be85e2c4df3c7730b3d9e40861d0ec8eca32960
alexkaufman06 <alexkaufman06@gmail.com>
2020-02-26 12:29:12 -0800

    Add initial files

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to tourOfHeroes!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Tour Of Heroes!');
   });
 });
