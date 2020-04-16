82640bc3c7341e4eac4afa92fce809528dd0d56f
Alex Kaufman <alexkaufman06@gmail.com>
2020-04-15 19:59:33 -0700

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,10 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('tourOfHeroes');
   });
-
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Tour Of Heroes!');
-  });
 });
